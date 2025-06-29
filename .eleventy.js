27ddbc477c9c4c4709ebd0fddc368e48f1bc76ca
Deyson <contact@deysonortiz.com>
2025-06-29 07:35:26 -0400

    Initial commit: Eleventy site with templated navigation and newsletter page

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  
+  return {
+    dir: {
+      input: "pages",
+      includes: "../_includes",
+      output: "_site"
+    },
+    // Specify template formats and engines
+    templateFormats: ["html", "njk", "md"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
